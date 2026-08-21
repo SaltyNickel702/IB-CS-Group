efa6f3a0bc96b22d230787f66bce9989a2c2e5d2
rabiescats <elliottmaw@gmail.com>
2026-08-21 13:59:24 -0600

    it

--- 2_Class_Prog/Item.h
+++ 2_Class_Prog/Item.h
@@ -2,16 +2,22 @@
 #define ITEM_H
 
 struct Item {
-    Item();
-
-    int baseDamage;
-    int damageModifier;
-    int mhealthModifier;
-    int heal;
-
     enum Types {
         //ex. Weapon, Potion...
+        weapon,
+        charm,
+        armor,
+        potion
     };
-};
 
+    Item();
+
+
+    Item::Types ItemType;
+
+    int baseDamage; //base damage of the item
+    int damageModifier; //damage modifier of the item
+    int maxHealthModifier; //max health modifier of the item
+    int heal; //Amount of health the item heals
+};
 #endif
