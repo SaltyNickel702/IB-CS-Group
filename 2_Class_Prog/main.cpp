4361b1be4c49a5a9222bcce5b301386ed748c546
Emil-Gruenwald <gruenwald.emil@gmail.com>
2026-08-21 14:10:34 -0600

    Update main.cpp

--- 2_Class_Prog/main.cpp
+++ 2_Class_Prog/main.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
-#include "player.h"
-#include "enemy.h"
+#include "Character.h"
+#include "Item.h"
+#include <vector>
+#include <string>
 
 int main () {
     return 0;
+
+    Character player = Character();
+    std::vector<Character> enemies;
+    std::string userInput;
+
+    while (player.health > 0) {
+        std::cout << "Select Action\n1:Attack\n2:Items\n";
+        std::cin >> userInput;
+
+        
+    };
 }
