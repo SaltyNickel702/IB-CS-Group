1f427fb56ec388e64ce2020bc291396defc30489
Emil-Gruenwald <gruenwald.emil@gmail.com>
2026-08-21 15:51:08 -0600

    Update main.cpp

--- 2_Class_Prog/main.cpp
+++ 2_Class_Prog/main.cpp
@@ -6,12 +6,11 @@
 
 int main()
 {
-    return 0;
-
     Character player = Character();
     std::vector<Character> enemies;
     std::string userInput;
     bool playerTurn = true;
+    int waves = 0;
 
     while (player.health > 0)
     {
@@ -19,43 +18,52 @@ int main()
         {
             std::cout << "Select Action\n1:Attack\n2:Items\n";
             std::cin >> userInput;
-            try
+
+            if (userInput == "1")
             {
-                if (userInput == "1")
+                std::cout << "Select Enemy to Attack\n";
+                for (int i = 0; i < enemies.size(); i++)
                 {
-                    std::cout << "Select Enemy to Attack\n";
-                    for (int i = 0; i < enemies.size(); i++)
-                    {
-                        // Print list of enemies
-                    }
-                    std::cin >> userInput;
-                    // Attack enemy
-                    playerTurn = false;
+                    // Print list of enemies
                 }
-                else if (userInput == "2")
+                std::cin >> userInput;
+                // Attack enemy
+                playerTurn = false;
+                if (enemies.size() == 0)
                 {
-                    std::cout << "Select Item to Use\n";
-                    for (int i = 0; i < player.potions.size(); i++)
-                    {
-                        // Print list of items
+                    std::cout << "New Enemies Appear\n";
+                    // Add new enemies to the vector
+                    playerTurn = true;
+                    waves++;
+
+                    if (waves == 10) {
+                        std::cout << "Victory!\n";
+                        return 0;
                     }
-                    std::cin >> userInput;
-                    // Use item
-                    playerTurn = false;
-                }
-                else
-                {
-                    throw std::invalid_argument("Invalid Input");
                 }
             }
-            catch (const std::invalid_argument &e)
+            else if (userInput == "2")
             {
-                
-            };
+                std::cout << "Select Item to Use\n";
+                for (int i = 0; i < player.potions.size(); i++)
+                {
+                    // Print list of items
+                }
+                std::cin >> userInput;
+                // Use item
+                playerTurn = false;
+            }
+            else
+            {
+                std::cout << "Invalid input.\n";
+            }
         };
+
         for (int i = 0; i < enemies.size(); i++)
         {
             // Enemy turn
         }
     };
+
+    return 0;
 }
